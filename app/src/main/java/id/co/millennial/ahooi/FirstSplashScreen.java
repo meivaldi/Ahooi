ae726e1698a9d259725583863fcaf60956608a67
andreepratama27 <andreepratama27@gmail.com>
2018-11-28 10:27:13 +0700

    Done

---
+++ app/src/main/java/id/co/millennial/ahooi/FirstSplashScreen.java
@@ -0,0 +1,41 @@
+package id.co.millennial.ahooi;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class FirstSplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_first_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), SecondSplashScreen.class));
+            }
+        }, 3000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        finish();
+    }
+}
--- app/src/main/java/id/co/millennial/ahooi/MainActivity.java
+++ app/src/main/java/id/co/millennial/ahooi/MainActivity.java
@@ -1,13 +1,49 @@
 package id.co.millennial.ahooi;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private Handler handler = new Handler();
+    private float progressStatus = 0;
+    private TextView textView;
+    private CircularProgressBar circularProgressBar;
+
+    private int time = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.timer);
+        circularProgressBar = (CircularProgressBar) findViewById(R.id.custom_progressBar);
+        circularProgressBar.setProgress(50.0f);
+
+        /*progressBar = (ProgressBar) findViewById(R.id.progressBar);*/
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 3.3f;
+                    handler.post(new Runnable() {
+                        public void run() {
+                            circularProgressBar.setProgress(progressStatus);
+                            textView.setText("" + time--);
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
     }
 }
